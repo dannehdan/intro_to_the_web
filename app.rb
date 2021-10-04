02d3d216cd95e1df2669f318d3f0fdd1c9eb8cea
Dan Marshall <daniel.o.marshall@gmail.com>
2021-10-04 16:07:31 +0100

    Added picture of a cat to path

--- app.rb
+++ app.rb
@@ -8,3 +8,13 @@ end
 get '/secret' do
   "This is a secret message"
 end
+
+get '/test' do
+  "Testing reloader"
+end
+
+get '/cat' do
+  "<div style='border: 2px dashed red'>
+  <img src='https://i.imgur.com/jFaSxym.png' alt='Picture of a cat'>
+  <div>"
+end
