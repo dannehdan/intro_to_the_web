080aa6cb4cac5c1c47dbaa604d392f65b6a9ae96
Dan Marshall <daniel.o.marshall@gmail.com>
2021-10-05 13:33:56 +0100

    Added variable to cat name

--- app.rb
+++ app.rb
@@ -14,7 +14,6 @@ get '/test' do
 end
 
 get '/cat' do
-  "<div style='border: 2px dashed red'>
-  <img src='https://i.imgur.com/jFaSxym.png' alt='Picture of a cat'>
-  <div>"
+  @name = ["Amigo", "Misty", "Almond"].sample
+  erb(:index)
 end
